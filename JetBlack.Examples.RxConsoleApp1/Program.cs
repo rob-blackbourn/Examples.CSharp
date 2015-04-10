39d24f81c55d66fce7be8fd8625243ff8abe4a89
Rob Blackbourn <rob.blackbourn@googlemail.com>
2015-04-10 10:10:22 +0100

    Initial check-in of module Examples.CSharp

---
+++ JetBlack.Examples.RxConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace JetBlack.Examples.RxConsoleApp1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Create<int>((observer, token) => 
+                Task.Factory.StartNew(() =>
+                    {
+                        var i = 0;
+                        while (!(token.WaitHandle.WaitOne(500) || token.IsCancellationRequested))
+                            observer.OnNext(i++);
+                    }, token));
+
+            var disposable = observable.Subscribe(
+                                 x => Console.WriteLine("OnNext: {0}", x),
+                                 error => Console.WriteLine("OnError: {0}", error),
+                                 () => Console.WriteLine("OnCompleted"));
+
+            Console.WriteLine("Press <ENTER> to stop subscriptions");
+            Console.ReadLine();
+
+            Console.WriteLine("Disposing");
+            disposable.Dispose();
+
+            Console.WriteLine("Press <ENTER> to quit");
+            Console.ReadLine();
+        }
+    }
+}
