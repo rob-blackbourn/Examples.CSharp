aba8e250604791e119771b76f85f0fe22459627a
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-04-27 08:58:49 +0100

    Removed a class from the client to make the operation more obvious.

--- JetBlack.Examples.SocketClient3/Program.cs
+++ JetBlack.Examples.SocketClient3/Program.cs
@@ -5,7 +5,6 @@ using System.Reactive.Linq;
 using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace JetBlack.Examples.SocketClient3
 {
@@ -22,52 +21,35 @@ namespace JetBlack.Examples.SocketClient3
                 Environment.Exit(-1);
             }
 
+            var host = splitArgs[0];
+            var port = int.Parse(splitArgs[1]);
+
             var cts = new CancellationTokenSource();
             var bufferManager = BufferManager.CreateBufferManager(2 << 16, 2 << 8);
 
-            using (var client = new EchoClient())
-            {
-                client.Dispatch(splitArgs[0], int.Parse(splitArgs[1]), bufferManager, cts.Token).Wait(cts.Token);
-            }
-        }
-    }
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(host, port);
 
-    class EchoClient : IDisposable
-    {
-        private readonly Socket _socket;
-
-        public EchoClient()
-        {
-            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        }
-
-        public async Task Dispatch(string hostname, int port, BufferManager bufferManager, CancellationToken token)
-        {
-            await _socket.ConnectAsync(hostname, port);
-
-            var sender = _socket.ToFrameSender(SocketFlags.None, token);
-            var receiver = _socket.ToFrameReceiver(SocketFlags.None, bufferManager);
-
-            var disposable = receiver
+            var receiverDisposable = socket.ToFrameReceiver(SocketFlags.None, bufferManager)
                 .SubscribeOn(TaskPoolScheduler.Default)
                 .Subscribe(x =>
                 {
                     Console.WriteLine("Read: " + Encoding.UTF8.GetString(x.Buffer, 0, x.Length));
                     x.Dispose();
-                });
+                },
+                error => Console.WriteLine("Error: " + error.Message));
 
-            for (var line = Console.ReadLine(); !token.IsCancellationRequested && !string.IsNullOrEmpty(line); line = Console.ReadLine())
+            var sender = socket.ToFrameSender(SocketFlags.None, cts.Token);
+
+            for (var line = Console.ReadLine(); !cts.IsCancellationRequested && !string.IsNullOrEmpty(line); line = Console.ReadLine())
             {
                 var writeBuffer = Encoding.UTF8.GetBytes(line);
                 sender.OnNext(new ManagedBuffer(writeBuffer, writeBuffer.Length, null));
             }
 
-            disposable.Dispose();
-        }
+            receiverDisposable.Dispose();
 
-        public void Dispose()
-        {
-            _socket.Close();
+            cts.Cancel();
         }
     }
 }
